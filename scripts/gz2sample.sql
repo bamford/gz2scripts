c38e0e1aa05fe6704f5962c052822e85f5a9e8a1
stevenbamford.net <stevenbamford.net@cf70db8c-d136-0410-8474-8200a805af0d>
2009-01-26 08:34:33 +0000

    Trying to reduce disk space usage: stage2 just contains extra cmodel columns, stage 3 back to being a view

--- scripts/gz2sample.sql
+++ scripts/gz2sample.sql
@@ -5,13 +5,27 @@
 --GO
 --DROP VIEW gz2sample_finaldr7comp
 
+GO
+SELECT objID
+INTO gz2sample_stage1dr7_objid_legacy
+FROM dr7.PhotoPrimary as P, dr7.FieldQA as F
+WHERE petroMag_r < 17.9 AND (psfMag_r - petroMag_r > 0.15)
+AND ((flags_r & 0x70000000) > 0) --BINNED1 OR BINNED2 OR BINNED4
+AND ((flags_r & 0x40000) = 0) --NOT SATURATED
+AND ((flags_r & 0x2) = 0) --NOT BRIGHT
+AND (((flags_r & 0x8) = 0) OR (flags_r & 0x40) > 0) --(NOT BLENDED OR NODEBLEND)
+--AND (b > 30.0) AND (b < -30)
+AND (P.fieldID = F.fieldID)
+AND (F.InLegacy = 1)
+-- DR6: 798683 objects
+
 -- get required data from PhotoPrimary
 -- reject objects which are flagged as
 -- SATURATED, BRIGHT, or (BLENDED and not NODEBLEND)
 -- also make any pre-selection cuts
 GO
 SELECT 
-objID, run, rerun, camcol, field, obj, ra, dec, petroR50_r, petroR90_r,
+G.objID, run, rerun, camcol, field, obj, ra, dec, petroR50_r, petroR90_r,
 petroMag_u, petroMag_g, petroMag_r, petroMag_i, petroMag_z,
 petroMagErr_u, petroMagErr_g, petroMagErr_r, petroMagErr_i, petroMagErr_z,
 psfMag_r, fiberMag_r, deVMag_r, deVMagErr_r, expMag_r, expMagErr_r, fracDeV_r,
@@ -19,34 +33,26 @@ psfMag_r, fiberMag_r, deVMag_r, deVMagErr_r, expMag_r, expMagErr_r, fracDeV_r,
 extinction_u, extinction_g, extinction_r, extinction_i, extinction_z,
 rowc_u, colc_u, rowc_g, colc_g, rowc_r,
 colc_r, rowc_i, colc_i, rowc_z, colc_z
-INTO gz2sample_stage1
-FROM dr7.PhotoPrimary
-WHERE petroMag_r < 17.9 AND (psfMag_r - petroMag_r > 0.15)
-AND ((flags_r & 0x70000000) > 0) --BINNED1 OR BINNED2 OR BINNED4
-AND ((flags_r & 0x40000) = 0) --NOT SATURATED
-AND ((flags_r & 0x2) = 0) --NOT BRIGHT
-AND (((flags_r & 0x8) = 0) OR (flags_r & 0x40) > 0) --(NOT BLENDED OR NODEBLEND)
--- DR6: 798683 objects
+INTO gz2sample_stage1_legacy
+FROM gz2sample_stage1dr7_objid_legacy as G, dr7.PhotoPrimary as P
+WHERE G.objid = P.objid
 
 -- calculate cmodel mags
 GO
-SELECT *,
+SELECT
 dbo.calc_cmodel(deVMag_r, expMag_r, fracDev_r, 1.2e-10) as cmodelMag_r,
 dbo.calc_cmodelerr(deVMagErr_r, expMagErr_r, deVMag_r, expMag_r,
     fracDev_r, 1.2e-10) as cmodelMagErr_r
-INTO gz2sample_stage2
-FROM mydb.gz2sample_stage1
-
-GO
-DROP TABLE gz2sample_stage1
+INTO gz2sample_stage2_legacy
+FROM mydb.gz2sample_stage1_legacy
 
 -- perform star-galaxy separation
 -- perform magnitude cut 
 -- perform surface brightness cut
 GO
-SELECT *
-INTO gz2sample_stage3
-FROM mydb.gz2sample_stage2
+CREATE VIEW gz2sample_stage3_legacy AS
+SELECT S1.*, S2.*
+FROM gz2sample_stage1_legacy as S1, gz2sample_stage2_legacy as S2
 WHERE (psfMag_r - cmodelMag_r >= 0.24)
 AND (petroMag_r <= 17.77)
 AND (mu50_r <= 23.0)
@@ -65,50 +71,47 @@ FROM
 (
     SELECT G.*, S.z as redshift, S.zErr as redshiftErr,
     ROW_NUMBER() OVER(PARTITION BY G.objid ORDER BY S.zErr) AS best
-    FROM mydb.gz2sample_stage3 as G
+    FROM mydb.gz2sample_stage3_legacy as G
     LEFT JOIN dr7.SpecObj as S on (S.bestObjID = G.objID)
 ) AS X
-INTO gz2sample_stage4
+INTO gz2sample_stage4_legacy
 WHERE best = 1
-
-GO
-DROP TABLE gz2sample_stage3
 
 ----------------------------------------------------------------------
 -- Determine effect of various potential further cuts to the sample
 ----------------------------------------------------------------------
 
 -- count objects with a redshift which implies it is in our own Galaxy
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE redshift < 0.0005
 -- DR6: 5331 objects (0.8%)
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE redshift < 0.001
 -- DR6: 5409 objects (0.8%)
 
 -- count objects with a redshift which implies
 -- it is too distant for useful classification
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE redshift > 0.25
 -- DR6: 4130 objects (0.6%)
 
 -- count objects smaller than 3 arcsec
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE petroR90_r < 3.0
 -- DR6: 21640 objects (3.3%)
 
 -- count objects smaller than 5 arcsec
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE petroR90_r < 5.0
 -- DR6: 195676 objects (30.0%)
 
 -- count objects fainter than r = 17.0
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE petroMag_r > 17.0
 -- DR6: 398650 objects (61.1%)
 
 -- count objects fainter than r = 16.5
-GO SELECT COUNT(*) FROM mydb.gz2sample_stage4
+GO SELECT COUNT(*) FROM mydb.gz2sample_stage4_legacy
 WHERE petroMag_r > 16.5
 -- DR6: 516206 objects (79.1%)
 
@@ -134,8 +137,8 @@ GO SELECT COUNT(*) FROM mydb.gz1_sdk50
 -- select objects brighter than r = 17 and larger than 3 arcsec
 GO
 SELECT *
-INTO gz2sample_finaldr7
-FROM mydb.gz2sample_stage4
+INTO gz2sample_finaldr7_legacy
+FROM mydb.gz2sample_stage4_legacy
 WHERE (petroMag_r - extinction_r) <= 17
 AND petroR90_r >= 3
 AND (((redshift > 0.0005) AND (redshift < 0.25)) OR redshift IS NULL)
@@ -145,8 +148,8 @@ AND objid NOT IN (SELECT objid FROM gz1_sdk80)
 -- comparative sample is built from a subset of main sample with
 -- redshifts.  Size and luminosity outliers are also excluded later.
 GO
-CREATE VIEW gz2sample_finaldr7comp AS
+CREATE VIEW gz2sample_finaldr7comp_legacy AS
 SELECT *
-FROM mydb.gz2sample_finaldr7
+FROM mydb.gz2sample_finaldr7_legacy
 WHERE redshift IS NOT NULL
 -- DR6:  174269 objects
