5e9c493510bcb6d49f6db89cf2aa678a279c70e7
Steven Bamford <steven@stevenbamford.com>
2011-08-11 22:01:06 +0100

    All set to get images for Darren Davis

--- get_gz2_images.py
+++ get_gz2_images.py
@@ -3,24 +3,22 @@
 import get_sdss_images
 import pyfits
 
-from get_gz2_data import *
-data = gz2data_dr7
-parents = gz2data_dr7_parents
+datafilename = 'gz2and1master.fits'
+data = pyfits.getdata(datafilename, 1)
 
 def selection():
-    s = N.zeros(len(data), N.bool)
-    s[:10000:100] = True
-    #run = data.field('run')
-    #s = s & (run > 1000) & (run < 5000)
+    s = data.field('arms_clean_union')
     return s
 
 def make_gz2_wget_list():
-    get_sdss_images.make_imaging_wget_list(dsel, bands=['r'], getmask=True, getatlas=True, getcat=False)
+    get_sdss_images.make_imaging_wget_list(data[selection()], bands=['r'],
+                                           getmask=True, getatlas=True, getcat=False)
 
 def get_gz2_images():
-    get_sdss_images.cut_out_objects(data[selection()], parents=parents, bands=['r'], clobber=False,
-                    getmask=True, getatlas=True, getparent=True,
-                    getclean=False, sizescale=6)
+    get_sdss_images.cut_out_objects(data[selection()],
+                                    parents=None, bands=['r'], clobber=False,
+                                    getmask=True, getatlas=True, getparent=True,
+                                    getclean=True, sizescale=10)
 
 if __name__ == "__main__":
     get_gz2_images()
