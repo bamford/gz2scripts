3658d66edf44925a55b9f1f4dc05a7231609f830
Steven Bamford <steven@stevenbamford.com>
2011-08-12 11:36:58 +0100

    Refined selection for getting gz2 images
    Does it in order of objid (i.e. all objects in same frame
    next to one another), and optionally limit to do in stages.

--- get_gz2_images.py
+++ get_gz2_images.py
@@ -6,9 +6,18 @@ import pyfits
 datafilename = 'gz2and1master.fits'
 data = pyfits.getdata(datafilename, 1)
 
-def selection():
-    s = data.field('arms_clean_union')
-    return s
+def selection(start=0, end=None):
+    # get objects to retrieve
+    flagged = data.field('arms_clean_union').nonzero()
+    idx = data.field('objid').argsort()
+    sel = flagged[idx]
+    n = len(sel)
+    print('Selected %i objects in total'%n)
+    if (end is None):
+        end = n
+    sel = sel[start:end]
+    print('Restricted selection to %i objects'%len(sel))
+    return sel
 
 def make_gz2_wget_list():
     get_sdss_images.make_imaging_wget_list(data[selection()], bands=['r'],
